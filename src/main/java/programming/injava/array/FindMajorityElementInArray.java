4447b680d2af0558c6e5c9d5d4a1f22466f98077
Deepak Kumar <2871622+dpkmcaanna@users.noreply.github.com>
2021-09-12 19:10:46 +0530

    Array-105 | ismajority ele

---
+++ src/main/java/programming/injava/array/FindMajorityElementInArray.java
@@ -0,0 +1,29 @@
+package programming.injava.array;
+
+public class FindMajorityElementInArray {
+
+	// find x is majority as n/2 element or not in sorted array
+	private static void isMajorityElement(int[] arr, int x) {
+		int len = arr.length;
+
+		// to point 2nd pointer as mid this mid index if len = 5, then mid = 2 [i.e 0,
+		// 1, 2]
+		// if len = 6, then mid = 3 [i.e 0, 1, 2, 3], fourth element
+		int mid = len / 2;
+		for (int i = 0; i < mid; i++) {
+			if (arr[i] == x && arr[i + mid] == x) {
+				System.out.println("True");
+				return;
+			}
+		}
+		System.out.println("False");
+	}
+
+	public static void main(String[] args) {
+		isMajorityElement(new int[] { 1, 1, 2, 2, 3 }, 2); // i = 1, mid = 2
+		isMajorityElement(new int[] { 1, 2, 3, 3, 3, 3, 10 }, 3);
+		isMajorityElement(new int[] { 1, 1, 2, 4, 4, 4, 6, 6 }, 4);
+		isMajorityElement(new int[] { 1, 1, 1, 2, 2 }, 1);
+	}
+
+}
