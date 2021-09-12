57246707748810f2f1fa39af0e73f8c828abaf94
Deepak Kumar <2871622+dpkmcaanna@users.noreply.github.com>
2021-09-12 07:00:17 +0530

    Array-105 | rotate array

--- src/main/java/programming/injava/array/RotateArrayByK.java
+++ src/main/java/programming/injava/array/RotateArrayByK.java
@@ -6,6 +6,7 @@ package programming.injava.array;
  * **/
 public class RotateArrayByK {
 
+	//In Anti clockwise
 	private static int[] roateByK(int[] arr, int k) {
 		int len = arr.length;
 		int[] rotatedArr =  new int[len];
@@ -15,6 +16,23 @@ public class RotateArrayByK {
 		}
 		
 		return rotatedArr;
+	}
+	
+	// In clock wise
+	private static void rotateArrayWithNoExtraSpace(int[] arr, int k) {
+		swap(arr, 0, arr.length - 1);
+		swap(arr, 0, k - 1);
+		swap(arr, k, arr.length - 1);
+	}
+	
+	private static void swap(int[] arr, int s, int e) {
+		while(s < e) {
+			arr[s] = arr[s] + arr[e];
+			arr[e] = arr[s] - arr[e];
+			arr[s] = arr[s] - arr[e];
+			s++;
+			e--;
+		}
 	}
 	
 	private static void printArray(int[] arr) {
@@ -27,5 +45,10 @@ public class RotateArrayByK {
 		int[] arr = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
 		int[] rotatedArr = roateByK(arr, 3);
 		printArray(rotatedArr);
+		
+		System.out.println();
+		
+		rotateArrayWithNoExtraSpace(arr, 4);
+		printArray(arr);
 	}
 }
