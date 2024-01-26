1131b3882e523dff9a1b9d89bed104880dbc7626
Deepak Kumar <2871622+dpkmcaanna@users.noreply.github.com>
2024-01-26 16:41:21 +0530

    added algot for Dutch national flag problem

--- src/main/java/programming/injava/array/SortBinary0s1s2s.java
+++ src/main/java/programming/injava/array/SortBinary0s1s2s.java
@@ -2,11 +2,24 @@ package programming.injava.array;
 
 public class SortBinary0s1s2s {
 	
+	/**
+	 * It's a Dutch National Flag problem, to solve this problem
+	 * User three pointer 
+	 * left pointing to 0s
+	 * mid pointing to 1s
+	 * right pointing to 2s
+	 * 
+	 * if mid == 0 then swap with left and increment both by one
+	 * if mid == 1 then only move mid by one
+	 * if mid == 2 then swap with right and then only decrement right by one
+	 * **/
+	
+	
 	public static void sort(int[] a) {
 		
 		int l = 0, h = a.length - 1, n = h;
 		
-		for(int i = 0; i < n && i <= h;) {
+		for(int i = 0; i < n && i <= h;) { // mid ~ i
 			if(a[i] == 0) {
 				swap(a, l, i);
 				l++;
