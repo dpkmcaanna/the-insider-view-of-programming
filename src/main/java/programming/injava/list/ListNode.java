0ab0bf2bcfde18daecf78fc80b039d6b890fbea1
Deepak Kumar <2871622+dpkmcaanna@users.noreply.github.com>
2024-01-28 15:38:03 +0530

    List-24.01.1 | Find cycle in List

--- src/main/java/programming/injava/list/ListNode.java
+++ src/main/java/programming/injava/list/ListNode.java
@@ -19,4 +19,28 @@ public class ListNode<T> {
 	ListNode(T data) {
 		this.data = data;
 	}
+	
+	public static <T> ListNode<T> buildLinkedList(T ...datas) {
+		ListNode<T> head = null, temp = null;
+		for(T data : datas) {
+			if(head == null) {
+				head = new ListNode<T>(data);
+				temp = head;
+			} else {
+				temp.setNext(new ListNode<T>(data));
+				temp = temp.getNext();
+			}
+		}
+		return head;
+	}
+	
+	public static <T> ListNode<T> getLastNode(ListNode<T> head) {
+		if(head == null) return head;
+		
+		while(head.getNext() != null) {
+			head = head.getNext();
+		}
+		
+		return head;
+	}
 }
